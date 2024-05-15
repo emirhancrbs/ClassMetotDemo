57955c53f4eb0b991fe8020404c0e6e1d356452e
emirhan <seraf@DESKTOP-FAA6MTS>
2024-05-15 23:58:19 +0300

    müşteri ekleme silme güncelleme

--- ClassMetotDemo/CustomerManager.cs
+++ ClassMetotDemo/CustomerManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,9 +14,33 @@ namespace ClassMetotDemo
             customers.Add(customer);
             Console.WriteLine(customer.Name+"Başarıyla Eklendi");
         }
-        public void DeleteCustomer(Customer customer,List<Customer> customers)
+        public void DeleteCustomer(int CustomeId,List<Customer> customers)
         {
+            customers.RemoveAt(CustomeId-1);
+            Console.WriteLine("Müşteri başarıyla silindi.");
+        }
+        public void UpdateCustomer(int customerId, List<Customer> customers)
+        {
+            ListCustomer(customers);
+            Console.Write("Güncellemek istediğiniz kullanıcının id sini giriniz = ");
+            int _custonmerId;
+            _custonmerId=Convert.ToInt32(Console.ReadLine());
+            foreach (var customer in customers)
+            {
+                if (_custonmerId==customer.Id)
+                {
+                    Console.WriteLine(customer.Name);
+                }
+            }
 
+
+        }
+        public void ListCustomer(List<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Id = "+ customer.Id+"\nName = "+ customer.Name+"\nSurname = "+ customer.Surname+"\nPhone Number = "+ customer.PhoneNumber+"E-mail = "+ customer.Email+"\n-----------------------------------------------------");
+            }
         }
     }
 }
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,2 +1,72 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ClassMetotDemo;
+
+CustomerManager customerManager= new CustomerManager();
+
+Customer customer=new Customer();
+
+customer.Id = 1;
+customer.Name = "Emirhan";
+customer.Surname = "Çeribaş";
+customer.Email = "emirhan@gmail.com";
+customer.PhoneNumber = "0(555) 555 55 55";
+
+Customer customer2 = new Customer();
+customer2.Id = 2;
+customer2.Name = "Yagmur";
+customer2.Surname = "Türkköylü";
+customer2.Email = "ygmr@gmail.com";
+customer2.PhoneNumber = "0(555) 555 55 55";
+
+Customer customer3 = new Customer();
+customer3.Id = 3;
+customer3.Name = "Barış";
+customer3.Surname = "Türkköylü";
+customer3.Email = "baris@gmail.com";
+customer3.PhoneNumber = "0(555) 555 55 55";
+
+List<Customer> customers = new List<Customer>();
+customers.Add(customer);
+customers.Add(customer2);
+customers.Add(customer3);
+
+Console.WriteLine("-------------------------------");
+Console.WriteLine("1-Müşteri Ekleme");
+Console.WriteLine("2-Müşteri Silme");
+Console.WriteLine("3-Müşteri Listeleme");
+Console.WriteLine("4-Müşteri Güncelleme");
+Console.Write("Lütfen Yapmak istediğiniz İşlemi Seçiniz = ");
+int process = Convert.ToInt32(Console.ReadLine());
+
+int customersLengt = customers.Count();
+Customer customer4 = new Customer();
+
+
+do
+{
+
+} while (true);
+switch(process)
+{
+    case 1:
+        customer4.Id = customersLengt + 1;
+        Console.Write("Müşteri Adını Giriniz = ");
+        customer4.Name = Console.ReadLine();
+        Console.Write("Müşteri  Soyadını Giriniz = ");
+        customer4.Surname = Console.ReadLine();
+        Console.Write("Müşteri  E-mail Giriniz = ");
+        customer4.Email = Console.ReadLine();
+        Console.Write("Müşteri  Telefon Numarasını Giriniz = ");
+        customer4.Email = Console.ReadLine();
+        customerManager.AddCustomer(customer4, customers);
+        break;
+    case 2:
+        customerManager.ListCustomer(customers);
+        Console.Write("Silinecek Müşterinin Id sini giriniz = ");
+        int customerId = Convert.ToInt32(Console.ReadLine());
+        customerManager.DeleteCustomer(customerId,customers );
+        break;
+    default:
+        Console.WriteLine("Lütfen bir işlem seçiniz.");
+        break;
+}
