8f38e8fffc49420369420a7915de60b324912fac
emirhan <seraf@DESKTOP-FAA6MTS>
2024-05-15 22:14:39 +0300

    deneme

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+        public void AddCustomer(Customer customer ,List<Customer> customers)
+        {
+            customers.Add(customer);
+            Console.WriteLine(customer.Name+"Başarıyla Eklendi");
+        }
+        public void DeleteCustomer(Customer customer,List<Customer> customers)
+        {
+
+        }
+    }
+}
